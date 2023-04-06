63ceb5d5c2a03db559142c671c5531b33e337fcf
Rifqaatifa02 <127828463+Rifqaatifa02@users.noreply.github.com>
2023-04-06 12:34:21 +0700

    Add files via upload

---
+++ Class.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mobil
+{
+    public class Class
+    {
+        private string Warna;
+        private int Pintu;
+        private string Merek;
+        private string Model;
+        private int Tahun;
+        private int Cepat;
+        private string Suara;
+
+        public string warna
+        {
+            get
+            {
+                return Warna;
+            }
+            set
+            {
+                Warna = value;
+            }
+        }
+        public int jumlahPintu
+        {
+            get
+            {
+                return Pintu;
+            }
+            set
+            {
+                Pintu = value;
+            }
+        }
+        public string merek
+        {
+            get
+            {
+                return Merek;
+            }
+            set
+            {
+                Merek = value;
+            }
+        }
+        public string model
+        {
+            get
+            {
+                return Model;
+            }
+            set
+            {
+                Model = value;
+            }
+        }
+        public int tahunKeluaran
+        {
+            get
+            {
+                return Tahun;
+            }
+            set
+            {
+                Tahun = value;
+            }
+        }
+        public int kecepatan
+        {
+            get
+            {
+                return Cepat;
+            }
+            set
+            {
+                Cepat = value;
+            }
+        }
+        public string suara
+        {
+            get
+            {
+                return Suara;
+            }
+            set
+            {
+                Suara = value;
+            }
+        }
+
+        public void Gas(int a)
+        {
+            Console.WriteLine("Mobil {0} berjalan dengan kecepatan {1}\n", model, a);
+        }
+        public void Klakson(string a)
+        {
+            Console.WriteLine("{0}\n", a);
+        }
+        public void TampilaknInfo()
+        {
+            Console.WriteLine("Mobil saya berwarna {0}, merek {1} model {2} keluaran tahun {3}" +
+                "dengan jumlah pintu sebanyak {4}", warna, merek, model, tahunKeluaran, jumlahPintu);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mobil
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Class mobil = new Class();
+
+            mobil.warna = "kuning";
+            mobil.jumlahPintu = 4;
+            mobil.merek = "Honda";
+            mobil.model = "Brio RS";
+            mobil.tahunKeluaran = 2022;
+
+            int kecepatan = mobil.kecepatan = 120;
+            string suara = mobil.suara = "Tiitttt";
+
+            mobil.Gas(kecepatan);
+            mobil.Klakson(suara);
+            mobil.TampilaknInfo();
+
+            Console.ReadKey();
+        }
+    }
+}
